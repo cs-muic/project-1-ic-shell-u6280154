38e11b0366311b78334d54272af1194c7fcff126
u6280154 <sumet.sae@student.mahidol.edu>
2022-09-27 23:19:03 +0700

    first commit

--- icsh.c
+++ icsh.c
@@ -1,17 +1,25 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Sumet Saelow
+ * StudentID: 6280154
  */
 
 #include "stdio.h"
+#include "stdlib.h"
 
 #define MAX_CMD_BUFFER 255
 
+char* read_line(){
+	char* buffer = (char*) malloc(MAX_CMD_BUFFER*sizeof(char));
+	fgets(buffer, 255, stdin) 
+	return buffer;
+}
+
 int main() {
-    char buffer[MAX_CMD_BUFFER];
+    char *buffer;
     while (1) {
         printf("icsh $ ");
-        fgets(buffer, 255, stdin);
+        buffer = read_line();
         printf("you said: %s\n", buffer);
+        free(buffer);
     }
 }
