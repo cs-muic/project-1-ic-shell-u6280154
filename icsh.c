81b141fa2b62185de14f3b55aad7a8e4a5fedebb
u6280154 <sumet.sae@student.mahidol.edu>
2022-09-28 13:31:34 +0700

    Make a modification

--- icsh.c
+++ icsh.c
@@ -5,21 +5,59 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 #define MAX_CMD_BUFFER 255
 
 char* read_line(){
 	char* buffer = (char*) malloc(MAX_CMD_BUFFER*sizeof(char));
-	fgets(buffer, 255, stdin) 
+	fgets(buffer, 255, stdin);
 	return buffer;
+}
+
+char** parse_line(char* line){
+	char** parse_list = malloc(10*sizeof(char*));
+	char* temp;
+	int i = 0;
+	temp = strtok(line," \t\r\n");
+	while(temp != NULL){
+		parse_list[i] = temp;
+		i++;
+		temp = strtok(NULL," \t\r\n");
+		}
+	return parse_list;
+	}
+
+int execute(char** arg){
+
+	if(!strcmp(arg[0],"echo")){
+		int i;
+		for(i = 1;arg[i] != NULL;i++){
+			printf("%s ",arg[i]);
+		}
+		printf("\n");
+		return 1;
+	}
+	else if(!strcmp(arg[0],"exit")){
+		printf("bye \n");
+		exit(arg[1]);
+	}
+	else{
+		printf("Command not found");
+	}
+	return 1;
 }
 
 int main() {
     char *buffer;
-    while (1) {
+    char **arg;
+    int status = 0;
+    do {
         printf("icsh $ ");
         buffer = read_line();
-        printf("you said: %s\n", buffer);
+        arg = parse_line(buffer);
+        status = execute(arg);
         free(buffer);
-    }
+        free(arg);
+    }while (status);
 }
